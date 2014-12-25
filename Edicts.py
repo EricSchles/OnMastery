6c727e3dca48b4af77d6d8f743f773d509b6f86b
EricSchles <Ericschles@gmail.com>
2014-12-25 10:55:47 -0500

    Create Edicts.py

---
+++ Edicts.py
@@ -0,0 +1,31 @@
+"""
+This file lays out my intensions for the library.  Rather than a library to be used for a specific framework or task, this library is
+intended to remind you why you develop.  Why you build.  Why you love what you do.  It is deeply philosophical and technically bland.
+Code is not the appropriate medium, from a conventional standpoint.  Which is why I write this as code.
+"""
+
+print "On Mastery"
+print "What are the edicts of master?"
+print "They are time dissimilar from love."
+print "I claim that love and mastery are one and the same - you cannot truly master a skill unless you love it."
+print "Perhaps you can be good at a task, even the best at it in all the world.  But unless you love the thing, you will never be a master."
+print "So what is my defense of such a bold statement?"
+
+print "To love something or someone is to know them deeply.  To enjoy and receive satisfaction from their 'company'."
+print "To know them - that is love"
+print "How can you have mastery without knowing the thing?"
+
+print "Clearly the expression of love with respect to a person and to an action or object are necessarily different."
+print "However I believe the similarities I stated above provide the correct recipe for comparison."
+
+print "And thus I claim all one need do to become a master of any task, skill, or way is to love it.  And I also claim that without love, you will never be a master, no matter how capable you are."
+print "This is because the one who loves a thing will do it even if they are immeasurably bad at it.  They will sit with the object of their love, regardless of how much they are not good.  And in this way they will put in the necessary time to achieve skill."
+print "They will want to learn everything about that other thing, and never shy away from the object of their affection, no matter what.  This is unconditional love - the unconditional love required to be a master."
+
+print "So why do I write this?"
+print "Because this is the way to becoming a master builder <=> a senior developer <=> one of the best computer scientists."
+print "This is how I rose from novice to my current station.  And this is why I will continue to rise.  Because I love making things."
+print "No matter how hard the task.  No matter how scary or daunting.  I will always try to do it as fast as possible, so I can learn more.  And I will always want more to do."
+print "This is because I love doing development."
+print "So even after a few years, you can imagine why I am as good as I am.  And why I am okay with not knowing everything.  Why I am excited every time someone tells me something I don't know."
+print "If we all came to programming with that genuine level of excitement and desire to learn.  Then there would be no limit to how much further programming could go."
